22a3e4c4789038dbfa827496f2cd1abcbcd05477
Carmine DiMascio <dimascio@us.ibm.com>
2016-09-08 19:55:09 -0400

    add ability to get icon name by icon code

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+var iconCodeMap = {
+  'tornado': ['0', '00'],
+  'tropical-storm': ['1', '01', '2', '02'],
+  'thunderstorm': ['3', '03', '4', '04'],
+  'rain-snow': ['5', '05', '7', '07'],
+  'rain-hail': ['6', '06', '10', '35'],
+  'freezing-drizzle': ['8', '08'],
+  'scattered-showers': ['9', '09', '11', '39'],
+  'rain': ['12'],
+  'flurries': ['13'],
+  'snow': ['14', '16'],
+  'blowing-snow': ['15', '25'],
+  'hail': ['17', '18'],
+  'fog': ['19', '20', '21', '22'],
+  'wind': ['23', '24'],
+  'cloudy': ['26'],
+  'mostly-cloudy-night': ['27'],
+  'mostly-cloudy': ['28'],
+  'partly-cloudy-night': ['29'],
+  'partly-cloudy': ['30'],
+  'clear-night': ['31'],
+  'sunny': ['32', '36'],
+  'mostly-clear-night': ['33'],
+  'mostly-sunny': ['34'],
+  'isolated-thunderstorms': ['37'],
+  'scattered-thunderstorms': ['38'],
+  'heavy-rain': ['40'],
+  'scattered-snow': ['41'],
+  'heavy-snow': ['42', '43'],
+  'na': ['-', '44', 'na'],
+  'scattered-showers-night': ['45'],
+  'scattered-snow-night': ['46'],
+  'scattered-thunderstorms-night': ['47']
+};
+
 module.exports = {
   getConditionByIconCode: function (iconCode) {
     var wxIcon = iconCode+'';
@@ -101,5 +136,19 @@ module.exports = {
       default:
         return {bucket: 'na', description: 'Not Available', timeframe: 'na'};
     }
+  },
+
+  getIconNameByIconCode: function(iconCode) {
+      var code = Number.parseInt(iconCode)
+      if (code === NaN || code < 0 || code > 47) {
+        return 'na';
+      }
+
+      for (var key in iconCodeMap) {
+        if (iconCodeMap[key].indexOf(code.toString()) !== -1) {
+          return key;
+        }
+      }
+      return 'na';
   }
 }
